7f68eddd7b1a8733a93c195f2ecbf80a574492ea
Daigo Shitara <stain.witness+github@gmail.com>
2022-06-21 14:16:48 +0900

    async basic

---
+++ 11_web_foundations/playbook/src/main.rs
@@ -0,0 +1,59 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+struct QuestionId(String);
+
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl std::fmt::Display for QuestionId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "id: {}", self.0)
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+impl std::fmt::Display for Question {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}, title: {}, content: {}, tags: {:?}",
+            self.id, self.title, self.content, self.tags
+        )
+    }
+}
+
+fn main() {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        String::from("First Question"),
+        String::from("Content of question"),
+        Some(vec![String::from("faq")]),
+    );
+    println!("{}", question);
+}
